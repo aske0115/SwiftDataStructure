2655921ab1ad7b0607c4b99c561d5a049e8948e0
Aske <burzum17@naver.com>
2022-01-04 20:35:48 +0900

    Initial Commit

---
+++ SwiftDataStructure/SwiftDataStructureApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataStructureApp.swift
+//  SwiftDataStructure
+//
+//  Created by GeunHwa Lee on 2022/01/04.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataStructureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
