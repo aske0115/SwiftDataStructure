e6fca474b8d956b2730f36a8a32e8644666e0b22
Aske <burzum17@naver.com>
2022-01-16 21:08:37 +0900

    Swift Data Structure  - Binary Tree   > Binary Search Tree  //tree 이론 공부 중 마음대로 짜본 것.  //애들 재우러 갈시간이라 일단끊고, 이론 다 듣고 다시 다듬을예정.

---
+++ SwiftDataStructure/Binary_Tree/BinarySearchTree.playground/Contents.swift
@@ -0,0 +1,88 @@
+class Node<T: Equatable & Comparable> : Equatable & Comparable {
+    var data: T
+    var left: Node?
+    var right: Node?
+    
+    init(item: T) {
+        self.data = item
+    }
+    
+    static func == (lhs: Node<T>, rhs: Node<T>) -> Bool {
+        return lhs.data == rhs.data &&
+                lhs.left == rhs.left &&
+                lhs.right == rhs.right
+    }
+    
+    static func < (lhs: Node<T>, rhs: Node<T>) -> Bool {
+        return lhs.data < rhs.data
+    }
+    
+    static func > (lhs: Node<T>, rhs: Node<T>) -> Bool {
+        return lhs.data > rhs.data
+    }
+}
+
+class BinarySearchTree<T: Equatable & Comparable> {
+    var root: Node<T>
+    
+    init(head: Node<T>) {
+        self.root = head
+    }
+    
+    func insert_data(item: T) {
+        var currentNode = root
+            while true {
+                //루트보다 넣을값이 크면 오른쪽으로..
+                if item > currentNode.data {
+                    if currentNode.right == nil {
+                        currentNode.right = Node<T>(item: item)
+                        break
+                    } else {
+                        currentNode = currentNode.right!
+                        continue
+                    }
+                } else if item < currentNode.data {
+                    if currentNode.left == nil {
+                        currentNode.left = Node<T>(item: item)
+                        break
+                    } else {
+                        currentNode = currentNode.left!
+                        continue
+                    }
+                }
+            }
+    }
+    
+    func search_data(item: T) -> Node<T>? {
+        if root.data == item {
+            return root
+        }
+        return nil
+    }
+    
+    func printTree() {
+        var rt: Node<T>? = root.left
+        while rt != nil {
+            print("rootData left: \(rt?.data)")
+            rt = rt?.left
+        }
+        
+        rt = root.right
+        while rt != nil {
+            print("rootData right: \(rt?.data)")
+            rt = rt?.right
+        }
+    }
+
+}
+
+
+//tree 공부하기 전에 느낌대로 만들어본 트리인데.. 애들재우러가야하니 이만.
+//tree공부 다 하고 다시 다듬어볼께요.
+
+let sol = BinarySearchTree<Int>(head: Node<Int>(item: 21))
+sol.insert_data(item: 14)
+sol.insert_data(item: 28)
+sol.insert_data(item: 11)
+sol.insert_data(item: 5)
+sol.printTree()
