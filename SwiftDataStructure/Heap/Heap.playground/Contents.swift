1d23b829cd4f3b221e438b100a1b3bf39cbebc91
Aske <burzum17@naver.com>
2022-01-23 22:38:14 +0900

    -Heap (Max Heap)  - done   - removeValue next time..

--- SwiftDataStructure/Heap/Heap.playground/Contents.swift
+++ SwiftDataStructure/Heap/Heap.playground/Contents.swift
@@ -6,19 +6,10 @@
  완전이진트리이기때문에 인덱스 규칙이 적용됨.
  */
 
-enum HeapType: Int {
-    case min,max
-}
 
 class Heap<T: Comparable> {
     var items:[T?] = []
-    private var heapType: HeapType = .max
-    
-    convenience init(_ item: T, heapType: HeapType) {
-        self.init(item)
-        self.heapType = heapType
-    }
-    
+
     init(_ item: T) {
         self.items.append(nil)
         self.items.append(item)
@@ -32,5 +23,37 @@ class Heap<T: Comparable> {
         }
         
         self.items.append(item)
+        
+        var currentInserted_idx = self.items.count - 1
+        while moveUp(currentInserted_idx) {
+            let parent_idx = currentInserted_idx / 2
+            let currentValue = self.items[currentInserted_idx]
+            self.items[currentInserted_idx] = self.items[parent_idx]
+            self.items[parent_idx] = currentValue
+            currentInserted_idx = parent_idx
+        }
     }
+    
+    private func moveUp(_ index: Int) -> Bool {
+        if  index <= 1 {
+            return false
+        }
+        
+        if let currentValue = self.items[index], let parent = self.items[index / 2] {
+            return currentValue > parent
+        }
+        return false
+    }
+    
 }
+
+let heap = Heap<Int>(15)
+heap.insert(10)
+heap.insert(8)
+heap.insert(5)
+heap.insert(4)
+
+print(heap.items)
+
+heap.insert(20)
+print(heap.items)
