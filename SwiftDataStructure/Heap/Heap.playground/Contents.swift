8e0f33e2497e0b487c97130c1e9db76ae5dd516c
Aske <burzum17@naver.com>
2022-01-23 23:11:19 +0900

    heap 부터 왜 집중이 하나도 안돼지.. 나중에 곱씹으며 다시 봐야할듯..

--- SwiftDataStructure/Heap/Heap.playground/Contents.swift
+++ SwiftDataStructure/Heap/Heap.playground/Contents.swift
@@ -45,6 +45,80 @@ class Heap<T: Comparable> {
         return false
     }
     
+    
+    func pop() -> T? {
+        
+        //root에서 데이터를 빼냄.
+        if self.items.count <= 1 {
+            return nil
+        }
+        
+        let popData = self.items[1]
+        self.items[1] = self.items[self.items.count - 1]
+        self.items.removeLast()
+        
+        var popIndex = 1
+        
+        while move_down(popIndex) {
+            var left_idx = popIndex * 2
+            var right_idx = popIndex * 2 + 1
+            if self.items.count < right_idx {
+                if self.items[popIndex]! < self.items[left_idx]! {
+                    let popValue = self.items[popIndex]
+                    self.items[popIndex] = self.items[left_idx]
+                    self.items[left_idx] = popValue
+                    popIndex  = left_idx
+                }
+            } else {
+                if self.items[left_idx]! > self.items[right_idx]! {
+                    if self.items[popIndex]! < self.items[left_idx]! {
+                        let popValue = self.items[popIndex]
+                        self.items[popIndex] = self.items[left_idx]
+                        self.items[left_idx] = popValue
+                        popIndex  = left_idx
+                    }
+                } else {
+                    if self.items[popIndex]! < self.items[right_idx]! {
+                        let popValue = self.items[popIndex]
+                        self.items[popIndex] = self.items[right_idx]
+                        self.items[right_idx] = popValue
+                        popIndex  = right_idx
+                    }
+                }
+            }
+        }
+        return popData
+    }
+    
+    private func move_down(_ index: Int) -> Bool {
+        let left_idx = index * 2
+        let right_idx = index * 2 + 1
+        
+        //왼쪽 자식노드가 없을때
+        if self.items.count <= left_idx {
+            return false
+        } else if self.items.count <= right_idx {
+            if self.items[index]! < self.items[left_idx]! {
+                return true
+            } else {
+                return false
+            }
+        } else {
+            if self.items[left_idx]! > self.items[right_idx]! {
+                if self.items[index]! < self.items[left_idx]! {
+                    return true
+                } else {
+                    return false
+                }
+            } else {
+                if self.items[index]! < self.items[right_idx]! {
+                    return true
+                } else {
+                    return false
+                }
+            }
+        }
+    }
 }
 
 let heap = Heap<Int>(15)
@@ -56,4 +130,6 @@ heap.insert(4)
 print(heap.items)
 
 heap.insert(20)
+print(heap.items)
+heap.pop()
 print(heap.items)
